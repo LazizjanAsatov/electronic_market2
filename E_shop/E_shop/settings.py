c6eafd3b8065b7110e49f3a35fb2d5244ef22551
Lazizjon <lazizjannsl12@gmail.com>
2024-08-14 22:32:46 +0500

    second Commit

--- E_shop/E_shop/settings.py
+++ E_shop/E_shop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qyuk6^*jj9#(x6z7_#a4%b07n9-k(yf&yh@qwnvj*c5v30o8@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
